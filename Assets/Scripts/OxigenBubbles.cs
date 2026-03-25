c27d0be7c22367a5f77a9104ed75b818953edf00
Nxvermind <leonardopacheco84@hotmail.com>
2026-03-25 17:43:21 -0500

    burbujas

---
+++ Assets/Scripts/OxigenBubbles.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxigenBubbles : MonoBehaviour
+{
+    [SerializeField] private Image oxygenBar;
+    [SerializeField] private RectTransform barRect;
+    [SerializeField] private RectTransform bubbles;
+
+    void Update()
+    {
+        float t = oxygenBar.fillAmount;
+
+        float minY = barRect.rect.yMin;
+        float maxY = barRect.rect.yMax;
+
+        float newY = Mathf.Lerp(minY, maxY, t);
+
+        Vector2 pos = bubbles.anchoredPosition;
+        pos.y = newY;
+        bubbles.anchoredPosition = pos;
+    }
+}
